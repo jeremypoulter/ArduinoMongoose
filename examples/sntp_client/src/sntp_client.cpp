809c61b567de8a00e71ff4d9730ffa2d92051bd5
Jeremy Poulter <jpoulter@webgains.com>
2024-08-09 22:09:48 +0100

    Updates to build on Linux

--- examples/sntp_client/src/sntp_client.cpp
+++ examples/sntp_client/src/sntp_client.cpp
@@ -5,7 +5,10 @@
 //  * handle missing pages / 404s
 //
 
+#ifdef ARDUINO
 #include <Arduino.h>
+#endif
+
 #include <MicroDebug.h>
 #include <MongooseCore.h>
 #include <MongooseSntpClient.h>
@@ -16,8 +19,6 @@
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #define START_ESP_WIFI
-#else
-#error Platform not supported
 #endif
 
 #ifndef SNTP_HOST
@@ -25,13 +26,42 @@
 //#define SNTP_HOST "time.google.com"
 #endif
 
+#ifndef LOGF
+#ifdef ARDUINO
+#define LOGF Serial.printf
+#else
+#define LOGF printf
+#endif
+#endif
+
+
 MongooseSntpClient sntp;
 
+#ifdef START_ESP_WIFI
 const char *ssid = "wifi";
 const char *password = "password";
+#endif
+
+const char *sntp_host = SNTP_HOST;
+
+int run = 1;
 
 static unsigned long next_time = 0;
 static bool fetching_time = false;
+
+#ifndef ARDUINO
+struct timespec millis_start_timestamp;
+
+unsigned long millis(void) 
+{
+    struct timespec timenow, start, end;
+    clock_gettime(CLOCK_REALTIME, &timenow);
+    start = millis_start_timestamp;
+    end = timenow;
+    // timeDiffmillis:
+    return ((end.tv_sec - start.tv_sec) * 1e3 + (end.tv_nsec - start.tv_nsec) * 1e-6);
+}
+#endif
 
 double diff_time(timeval tv1, timeval tv2)
 {
@@ -41,9 +71,10 @@ double diff_time(timeval tv1, timeval tv2)
     return t1-t2;
 }
 
-
 void setup()
 {
+#ifdef ARDUINO
+
   Serial.begin(115200);
 
 #ifdef START_ESP_WIFI
@@ -65,6 +96,9 @@ void setup()
   Serial.println(WiFi.hostname());
 #endif
 #endif
+#else
+  clock_gettime(CLOCK_REALTIME, &millis_start_timestamp);
+#endif
 
   Mongoose.begin();
 
@@ -85,18 +119,45 @@ void loop()
   {
     fetching_time = true;
 
-    DBUGF("Trying to get time from " SNTP_HOST);
-    sntp.getTime(SNTP_HOST, [](struct timeval server_time)
+    DBUGF("Trying to get time from %s", sntp_host);
+    sntp.getTime(sntp_host, [](struct timeval server_time)
     {
       struct timeval local_time;
       gettimeofday(&local_time, NULL);
-      Serial.printf("Local time: %s", ctime(&local_time.tv_sec));
-      Serial.printf("Time from %s: %s", SNTP_HOST, ctime(&server_time.tv_sec));
-      Serial.printf("Diff %.2f\n", diff_time(server_time, local_time));
+      LOGF("Local time: %s", ctime(&local_time.tv_sec));
+      LOGF("Time from %s: %s", sntp_host, ctime(&server_time.tv_sec));
+      LOGF("Diff %.2f\n", diff_time(server_time, local_time));
       settimeofday(&server_time, NULL);
       
       fetching_time = false;
       next_time = millis() + 10 * 1000;
+      run--;
     });
   }
 }
+
+
+#ifndef ARDUINO
+int main(int argc, char *argv[])
+{
+  int i;
+
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--help") == 0) {
+      fprintf(stderr, "Usage: %s [host]\n", argv[0]);
+      exit(EXIT_SUCCESS);
+    } else {
+      break;
+    }
+  }
+
+  if (i + 1 == argc) {
+    sntp_host = argv[i];
+  }
+
+  setup();
+  while(run) {
+    loop();
+  }
+}
+#endif
